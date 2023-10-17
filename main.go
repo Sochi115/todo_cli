85f890e7d679bcbec90127c7724e5b3f4ed77ca0
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2023-10-17 21:59:18 +0700

    Added command line flags

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"time"
+	"os"
 
 	"github.com/Sochi115/todo_cli/app"
 )
 
-func main() {
-	start := time.Now()
-	// app.GetTodos()
-	var app app.App
-	// app.JsonFileName = "resources/dummyJson.json"
-	app.JsonFileName = "dummyJson.json"
+var appInstance app.App
 
-	app.GetTodos()
-	app.ParseToTable()
-	fmt.Println(time.Since(start))
+func main() {
+	if len(os.Args) < 2 {
+		printTable()
+	} else {
+		handleArgs()
+	}
+}
+
+func handleArgs() {
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	addTaskName := addCmd.String("task", "", "Adds task")
+	addIsHigh := addCmd.Bool("isHigh", false, "Sets priority of the task")
+
+	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+	deleteId := deleteCmd.Int("id", -1, "Deletes task by id")
+
+	switch os.Args[1] {
+	case "add":
+		addCmd.Parse(os.Args[2:])
+		fmt.Println("Task: ", *addTaskName)
+		fmt.Println("Is High Prio: ", *addIsHigh)
+
+	case "delete":
+		deleteCmd.Parse(os.Args[2:])
+		fmt.Println("ID: ", *deleteId)
+
+	default:
+		printTable()
+	}
+}
+
+func printTable() {
+	appInstance.JsonFileName = "resources/dummyJson.json"
+	appInstance.GetTodos()
+	appInstance.ParseToTable()
 }
